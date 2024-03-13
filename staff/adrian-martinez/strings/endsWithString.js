3bcb6fe2162995f0ec044edec2cd51ae2cae6079
Strider <adrian.martinez89@gmail.com>
2024-03-13 23:50:58 +0100

    Update endsWithString.js #153

--- staff/adrian-martinez/strings/endsWithString.js
+++ staff/adrian-martinez/strings/endsWithString.js
@@ -10,7 +10,7 @@ function endsWithString(string, buscaCaracter){
             }
         }
     }
-    if(acumulate == buscaCaracter){
+    if(acumulate === buscaCaracter){
         return true;
     }
     
@@ -22,3 +22,10 @@ var cadena = "El desarrollo web es el santo grial de todo negocio que quiera cre
 var result = endsWithString(cadena, "crecer.");
 
 console.log({expected: true, received: result});
+
+console.log("CASO 2: La función devuelve verdadero si la cadena pasada se encuentra en esa longitud.\n");
+var cadena = "El desarrollo web es el santo grial de todo negocio que quiera crecer.";
+var result = endsWithString(cadena, "crecer.", 40);
+
+console.log({expected: true, received: result});
+
