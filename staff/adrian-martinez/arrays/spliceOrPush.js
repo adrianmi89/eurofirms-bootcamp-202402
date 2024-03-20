9f8e0388c71486d04df5ad6bf111b1381a6892a0
Strider <adrian.martinez89@gmail.com>
2024-03-20 23:59:05 +0100

    Add spliceOrPush update #177

--- staff/adrian-martinez/arrays/spliceOrPush.js
+++ staff/adrian-martinez/arrays/spliceOrPush.js
@@ -3,9 +3,9 @@
 // si peter ya lo tiene guardado, deberia borrarse el id del array
 // si no lo tiene, deberia agregarlo
 
-// para busar al usuario, utilizar el id
+// para buscar al usuario, utilizar el id
 
-// hacer dos casos, uno para cuando lo encuentra en el array, otro para cuando no lo encuentra
+// hacer dos casos, uno para cuando lo encuentre en el array, otro para cuando no lo encuentre
 
 // metodos sugeridos a usar: find, includes, splice, push 
 // (puedes agregar otros o prescindir de alguno si lo ves necesario, splice y push son obligatorios)
@@ -16,3 +16,36 @@ var users = [
     { id: '5t45e5i', name: 'peter', email: 'peter@pan.com', saved: ['grghejh4', '4tw4hj4', 'f4jkg4g', 'f4wfjw4f'] },
     { id: 'djn5gje', name: 'pinocho', email: 'pin@ocho.com', saved: [] },
 ]
+
+var encuentraUsuario = users.find(function (user){
+    return user.id == "5t45e5i";
+})
+//console.log(encuentraUsuario);
+
+var {id, saved} = encuentraUsuario;
+
+var addID;
+var modificaPost = saved.forEach(function (elemento){
+    
+    if(elemento == id){
+
+        addID = id;
+    }
+    
+    return elemento == id;
+})
+console.log(modificaPost);
+
+//Si el id está en la lista lo borramos y si no está se lo añadimos al final
+if(modificaPost !== undefined){
+    saved.splice(addID, 1);
+}
+else{
+    saved.push(id);
+}
+
+console.log("\nCASE 1: Debemos buscar el post con el id guardado en el objeto de peter y si existe borrarlo.\n");
+console.log({expected: "{saved: ['grghejh4', '4tw4hj4', 'f4jkg4g', 'f4wfjw4f]", received: saved});
+
+console.log("\nCASE 2: Si no existe el post debemos añadirlo en el array de saved.\n");
+console.log({expected: "saved: ['grghejh4', '4tw4hj4', 'f4jkg4g', 'f4wfjw4f,'5t45e5'] ", received: saved});
